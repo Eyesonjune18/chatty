746305d76bb5aaf40adfb118ba9de4e50e8a7030
Lowell Thoerner <lowellashton@gmail.com>
2023-01-29 15:51:07 -0800

    Added some imports.

--- src/receiver.rs
+++ src/receiver.rs
@@ -1,21 +1,20 @@
-use std::io::{self, Read};
+use std::io::{self, BufReader, BufRead};
 use std::net::TcpListener;
 
 #[allow(dead_code)]
 pub fn start() -> io::Result<()> {
     let listener = TcpListener::bind("192.168.137.191:8095")?;
-    let mut client = listener.accept()?.0;
+    let client = listener.accept()?.0;
+    let mut reader = BufReader::new(client);
     
-    let mut buffer = [0; 1024];
-
     loop {
-        let num_read = client.read(&mut buffer)?;
+        let mut message = String::new();
+        let num_read = reader.read_line(&mut message)?;
 
         if num_read == 0 {
             return Ok(());
         }
 
-        let message = String::from_utf8_lossy(&buffer[..num_read]);
         print!("Received message: {}", message);
     }
 }
