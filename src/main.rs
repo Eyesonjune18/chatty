ac1051ff755dcfd323ac51c258df4ba9c13fc611
Lowell Thoerner <lowellashton@gmail.com>
2023-01-29 16:25:49 -0800

    Working on threading for rewrite.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
-mod sender;
-mod receiver;
+use std::io::{self, Write, BufRead, BufReader};
+use std::net::{TcpStream, TcpListener};
 
-fn main() -> std::io::Result<()> {
-    sender::start()?;
-    // receiver::start()?;
+fn main() {
+    start_sender();
+    start_receiver();
+}
 
-    Ok(())
+fn start_sender() {
+    std::thread::spawn(|| {
+        // Connect to the server at 192.168.137.191:9000
+        let mut stream = TcpStream::connect("192.168.137.191:9001").unwrap();
+        
+        loop {
+            // Read a line from stdin
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            
+            // Write the line to the server
+            stream.write(input.as_bytes()).unwrap();
+        }
+    });
+}
+
+fn start_receiver() {
+    std::thread::spawn(|| {
+        // Open connections on port 9001
+        let listener = TcpListener::bind("192.168.137.102:9001").unwrap();
+
+        // Accept a connection
+        let (stream, _) = listener.accept().unwrap();
+        // Read from the connection
+        let mut reader = BufReader::new(stream);
+
+        loop {
+            let mut line = String::new();
+            reader.read_line(&mut line).unwrap();
+            println!("R: {}", line);
+        }
+    });
 }
